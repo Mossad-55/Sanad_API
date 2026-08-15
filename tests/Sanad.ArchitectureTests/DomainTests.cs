616c5b3b3419cf1032ba4b14d6265efa94b6fb0e
Mossad <mosad55522@gmail.com>
2026-08-15 20:46:49 +0300

    test: create testing foundation

---
+++ tests/Sanad.ArchitectureTests/DomainTests.cs
@@ -0,0 +1,20 @@
+﻿using FluentAssertions;
+using NetArchTest.Rules;
+
+namespace Sanad.ArchitectureTests;
+
+public class DomainTests
+{
+    [Fact]
+    public void Domain_Should_Not_Depend_On_MediatR()
+    {
+        var result = Types.InCurrentDomain()
+            .That()
+            .ResideInNamespace("Sanad.BuildingBlocks.Domain")
+            .ShouldNot()
+            .HaveDependencyOn("MediatR")
+            .GetResult();
+
+        result.IsSuccessful.Should().BeTrue();
+    }
+}
