e1282bf8d7dfbb8974ba60c5387879dfe4d8e7d4
Mossad <mosad55522@gmail.com>
2026-08-19 00:59:04 +0300

    refactor(domain): standardize empty id properties

--- src/BuildingBlocks/Domain/Sanad.BuildingBlocks.Domain/Primitives/Ids/DeviceSessionId.cs
+++ src/BuildingBlocks/Domain/Sanad.BuildingBlocks.Domain/Primitives/Ids/DeviceSessionId.cs
@@ -3,6 +3,6 @@ namespace Sanad.BuildingBlocks.Domain.Primitives.Ids;
 public readonly record struct DeviceSessionId(Guid Value)
 {
     public static DeviceSessionId New() => new(Guid.CreateVersion7());
-    public static DeviceSessionId Empty() => new(Guid.Empty);
+    public static DeviceSessionId Empty => new(Guid.Empty);
     public override string ToString() => Value.ToString();
 }
