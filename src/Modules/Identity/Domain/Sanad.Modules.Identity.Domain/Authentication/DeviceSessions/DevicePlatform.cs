fac45ebe8c115dfc8efaee4b6a6a7413f5f70dc3
Mossad <mosad55522@gmail.com>
2026-08-19 00:49:40 +0300

    refactor(identity): correct device session naming

--- src/Modules/Identity/Domain/Sanad.Modules.Identity.Domain/Authentication/DeviceSessions/DevicePlatform.cs
+++ src/Modules/Identity/Domain/Sanad.Modules.Identity.Domain/Authentication/DeviceSessions/DevicePlatform.cs
@@ -2,7 +2,7 @@ namespace Sanad.Modules.Identity.Domain.Authentication.DeviceSessions;
 
 public enum DevicePlatform
 {
-    Unkown = 0,
+    Unknown = 0,
     Android = 1,
     iOS = 2,
     Web = 3
